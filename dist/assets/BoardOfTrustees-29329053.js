679665b83833986b985fd0567aebd38047ba3644
joecaporiccio <joseph.caporiccio@envisionpharmagroup.com>
2023-10-02 08:22:45 -0400

    add dist subtree

---
+++ dist/assets/BoardOfTrustees-29329053.js
@@ -0,0 +1,1 @@
+import{o as e,c as s,a,F as r,_ as t,b as _,d as c}from"./index-3e0e8090.js";const d={__name:"BoardOfTrustees",setup(n){return(o,i)=>(e(),s(r,null,[a(t),a(_,{"has-divider":!0}),a(c,{"has-divider":!0})],64))}};export{d as default};
